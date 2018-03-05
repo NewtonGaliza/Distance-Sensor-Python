c76942591aa283de6df4fab9055de56bad0c3aa0
Newton Galiza <newtonjgaliza@gmail.com>
2018-03-05 19:31:44 -0300

    Update regua.py

--- regua.py
+++ regua.py
@@ -6,6 +6,5 @@ arduinoSerialData = serial.Serial('/dev/ttyUSB0', 9600)
 while (1==1):
     if (arduinoSerialData.inWaiting() > 0):
         myData = arduinoSerialData.readline()
-#        distance = float(myData)
-#       print(distance)
-        print(type(myData))
+        distance = float(myData)
+        print(distance)
